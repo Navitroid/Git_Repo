e6ba08414a11f6aac5dc285b35dbc905fdb9f967
Navitroid <84046525+Navitroid@users.noreply.github.com>
2021-12-09 15:39:08 +0300

    Create proj_2.py

---
+++ proj_2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import scipy.stats as ss
+import seaborn as sns
+import matplotlib as plt
+import numpy as np
+
+df = pd.read_csv('//home/jupyter-i.netreba-11/Mat_stat/conversion.csv')
